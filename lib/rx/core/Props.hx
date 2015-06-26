8b1ebbd4fcd3f11a200621bc6dead4cbe249858f
Francesco Agati <fagati@veespo.com>
2015-06-26 21:08:31 +0200

    inline methods

--- lib/rx/core/Props.hx
+++ lib/rx/core/Props.hx
@@ -1,11 +1,36 @@
 package rx.core;
 
+
+class Helper {
+  public static inline function deleteField(o,field) {
+    untyped __js__('delete({0}[{1}]);',o,field);
+    return true;
+  }
+
+  public static inline function fields(obj) {
+    return untyped __js__('Object.keys({0})',obj);
+  }
+
+}
+
 abstract Props({}) from {} {
   inline function new(a:Dynamic)
     this = a;
   public inline function get(name: String):Dynamic return untyped this[name];
-    public inline function keys():Array<String> return Reflect.fields(this);
+    public inline function keys():Array<String> {
+      #if js
+        return Props.Helper.fields(this);
+      #else
+        return Reflect.fields(this);
+      #end
+    }
     public inline function set(name: String, value: Dynamic):Void untyped this[name] = value;
     public inline function exists(name: String):Bool return untyped this[name] != undefined;
-    public inline function remove(name: String) untyped Reflect.deleteField(this, name);
+    public inline function remove(name: String) {
+        #if js
+          Props.Helper.deleteField(this,name);
+        #else
+          untyped Reflect.deleteField(this, name);
+        #end
+    }
 }
